0e45c4aae0ffb01db0d144f4736408ba41544213
Redhouane Sobaihi <redouane.sobaihi@live.com>
2023-11-13 16:32:35 +0000

    Generate SDK updates

---
+++ src/models/OrganizationLocation.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type OrganizationLocation = {
+    longitude: number;
+    latitude: number;
+    address?: string | null;
+    entrance?: string | null;
+    floor?: string | null;
+    office?: string | null;
+};
+
---
+++ src/services/BookingManager.ts
@@ -0,0 +1,83 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { BookingsPageResponse } from '../models/BookingsPageResponse';
+import type { CreateBookingParams } from '../models/CreateBookingParams';
+import type { EmployeeBookingResponse } from '../models/EmployeeBookingResponse';
+import type { UpdateEmployeeBookingParams } from '../models/UpdateEmployeeBookingParams';
+
+import type { CancelablePromise } from '../core/CancelablePromise';
+import type { BaseHttpRequest } from '../core/BaseHttpRequest';
+
+export class BookingManager {
+
+    constructor(public readonly httpRequest: BaseHttpRequest) {}
+
+    /**
+     * List employee bookings for current active organization
+     * @param page
+     * @param pageSize
+     * @param serviceId
+     * @param employeeId
+     * @returns BookingsPageResponse
+     * @throws ApiError
+     */
+    public list(
+        page: number,
+        pageSize: number,
+        serviceId?: string,
+        employeeId?: string,
+    ): CancelablePromise<BookingsPageResponse> {
+        return this.httpRequest.request({
+            method: 'GET',
+            url: '/booking-manager',
+            query: {
+                'serviceId': serviceId,
+                'employeeId': employeeId,
+                'page': page,
+                'pageSize': pageSize,
+            },
+        });
+    }
+
+    /**
+     * Create employee booking object
+     * @param requestBody
+     * @returns EmployeeBookingResponse Booking object created successfully
+     * @throws ApiError
+     */
+    public bookingManagerControllerCreate(
+        requestBody: CreateBookingParams,
+    ): CancelablePromise<EmployeeBookingResponse> {
+        return this.httpRequest.request({
+            method: 'POST',
+            url: '/booking-manager',
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+
+    /**
+     * Update employee booking object
+     * @param id
+     * @param requestBody
+     * @returns any Booking object updated successfully
+     * @throws ApiError
+     */
+    public bookingManagerControllerUpdate(
+        id: string,
+        requestBody: UpdateEmployeeBookingParams,
+    ): CancelablePromise<any> {
+        return this.httpRequest.request({
+            method: 'PATCH',
+            url: '/booking-manager/{id}',
+            path: {
+                'id': id,
+            },
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+
+}
