f78ed0e75024d9182259a9c582e378024b37aa02
Redhouane Sobaihi <redouane.sobaihi@live.com>
2023-11-15 15:16:27 +0000

    Generate SDK updates

---
+++ src/models/BookingsPageResponse.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { EmployeeBookingResponse } from './EmployeeBookingResponse';
+
+export type BookingsPageResponse = {
+    total: number;
+    hasMore: boolean;
+    data: Array<EmployeeBookingResponse>;
+};
+
---
+++ src/models/CreateBookingParams.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type CreateBookingParams = {
+    employeeId: number;
+    service: string;
+    startTime: string;
+    endTime: string;
+    days: number;
+    metadata: Record<string, any>;
+};
+
