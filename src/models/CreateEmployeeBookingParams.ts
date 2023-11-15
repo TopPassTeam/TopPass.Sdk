41edf9e45fe64618d16b354d42ae13583187ff63
Redhouane Sobaihi <redouane.sobaihi@live.com>
2023-11-15 16:00:34 +0000

    release: generate release 20231115.160034

---
+++ src/models/CreateEmployeeBookingParams.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type CreateEmployeeBookingParams = {
+    employeeId: number;
+    startTime: string;
+    endTime: string;
+    days: number;
+};
+
---
+++ src/models/EmployeeOrganizationResponse.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type EmployeeOrganizationResponse = {
+    organizationId: string;
+    organizationName: string;
+    isActive: boolean;
+    roles: Array<number>;
+};
+
