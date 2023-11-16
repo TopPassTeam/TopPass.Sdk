36bfa1afb6bb3c8a91a93bc8a57dfc87774263aa
redouane <redouane.sobaihi@live.com>
2023-11-16 03:03:56 +0300

    fix: build for es module and commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,34 @@
 import typescript from '@rollup/plugin-typescript';
-import dts from "rollup-plugin-dts";
+import * as d from "rollup-plugin-dts";
 
 const config = [
   {
     input: 'build/src/index.js',
     output: {
-      file: 'dist/index.js',
+      file: 'dist/cjs/index.js',
       format: 'cjs',
       sourcemap: true,
     },
     external: ['axios', 'form-data'],
     plugins: [typescript({ declaration: false })]
-  }, {
+  }, 
+  {
+    input: 'build/src/index.js',
+    output: {
+      file: 'dist/mjs/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    external: ['axios', 'form-data'],
+    plugins: [typescript({ declaration: false })]
+  },
+  {
     input: 'build/src/index.d.ts',
     output: {
       file: 'dist/index.d.ts',
       format: 'es'
     },
-    plugins: [dts()]
+    plugins: [d.dts()]
   }
 ];
 
